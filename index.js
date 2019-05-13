6b42ef609bc99f433ba94359d96a6ecbe055d69a
Jacob Lam <jlam18@binghamton.edu>
2019-05-13 11:53:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(drivers){
+  return Object.assign(drivers.map(function(name){
+    return name.toLowerCase();
+  }))
+}
+
+function nameToAttributes(arr){
+  return arr.map(function(name){
+    const firstName = name.split(" ")[0]
+    const lastName = name.split(" ")[1]
+    return {firstName: firstName , lastName: lastName}
+  })
+}
+
+function attributesToPhrase(arr){
+  return arr.map(function(person){
+    return `${person.name} is from ${person.hometown}`
+  })
+}
